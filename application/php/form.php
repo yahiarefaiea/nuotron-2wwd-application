202cb55706e9611939b93820f7c4fcc5ec3f41b1
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-07-31 21:59:52 +0300

    finalize form.php

--- application/php/form.php
+++ application/php/form.php
@@ -6,32 +6,33 @@ if (isset($_POST["dd"])) {
   $headers.= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
 	//  USER VARIABLES
-	$userName= "Someone";
-	// $userName= $data[12]->answer[0]->answer;
-	// $userEmail= $data[12]->answer[1]->answer;
-  // $userSubject= "Team Nuotron - Thanks for your help";
+	$userName= $data[0]->answer;
+	$userEmail= $data[2]->answer;
+  $userSubject= "Yahia Refaiea - Thanks for joining our two weeks web design";
 
 	//  MY VARIABLES
 	$myName= "Yahia Refaiea";
 	$myEmail= "2wwd@nuotron.co";
 
-	// if ($userName === 'Passed empty..') {
-	// 	$userName= "";
-	// }
+	if ($userName === 'Passed empty..') {
+		$userName= "";
+	}
 	//	USER MESSAGE
-  $userMessage= '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/><title>Nuotron 2WWD Survey - New Feedback Received</title></head><body style="margin: 0;"><table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" style="padding: 42px 14px 56px 14px; background-color: #eee;"><tr><td align="left" valign="top"><table border="0" cellpadding="0" cellspacing="0" style="min-width: 400px; width: 100%; max-width: 560px; margin: 0 auto;"><tr><td align="left" valign="top"><table border="0" cellpadding="0" cellspacing="0" width="100%" style="margin-bottom: 28px; padding: 0 56px 0 56px;"><tr><td align="left" valign="top"><a href="https://2wwd.nuotron.co/survey/" target="_blank" style="display: block; text-decoration: none; width: 70px;"><img src="https://2wwd.nuotron.co/survey/includes/images/mails/nuotron.png" height="70px" style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 700; font-size: 14px; line-height: 70px; color: #727272; white-space: nowrap;" alt="Nuotron Logo" title="Nuotron Logo"/></a></td></tr><tr><td align="left" valign="top" style="padding-top: 21px;"><span style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 400; font-size: 16px; line-height: 28px; color: #464646;"><span>Hello <span style="font-weight: 700;">Yahia Refaiea</span>,<br/>You have a new feedback to look at</span></span></td></tr></table><table border="0" cellpadding="0" cellspacing="0" width="100%" style="background-color: #f35635;"><tr><td align="center" valign="top"><img src="https://2wwd.nuotron.co/survey/includes/images/mails/nuotron-cover.png" width="100%" style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 700; font-size: 28px; line-height: 210px; color: rgba(0, 0, 0, .21);" alt="Nuotron Cover" title="Nuotron Cover"/></td></tr></table><table border="0" cellpadding="0" cellspacing="0" width="100%" style="margin-bottom: 21px; padding: 28px 56px 28px 56px; background-color: #fff;"><tr><td align="left" valign="top"><span style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 400; font-size: 14px; line-height: 28px; color: #727272;"><span style="display: block; margin-bottom: 21px;">Sed Do has just sent a feedback to the Nuotron 2WWD survey.</span><span style="display: block; margin-bottom: 7px; font-size: 12px; line-height: 21px; color: #929292;">01. Overall, how are you feeling about Nuotron?</span><span style="display: block; margin-bottom: 21px; color: #545454;">Satisfied</span><span style="display: block; margin-bottom: 7px; font-size: 12px; line-height: 21px; color: #929292;">02. How did you spell Nuotron for the first time?</span><span style="display: block; margin-bottom: 21px; color: #545454;">Nokbok</span><span style="display: block;">Keep up the good work,<br/>Team Nuotron</span></span></td></tr></table><table border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td align="center" valign="top"><a href="https://2wwd.nuotron.co/survey/" target="_blank" style="display: block; text-decoration: none; width: 141px;"><img src="https://2wwd.nuotron.co/survey/includes/images/mails/nuotron-flat.png" height="35px" style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 700; font-size: 14px; line-height: 35px; color: #727272;" alt="Nuotron Logo" title="Nuotron Logo"/></a></td></tr><tr><td align="center" valign="top" style="padding-top: 3px;"><span style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 400; font-size: 12px; line-height: 21px; color: #929292;">reach me out through: <br/><a href="https://yahiarefaiea.com" target="_blank" style="color: #929292;">yahiarefaiea.com</a></span></td></tr></table></td></tr></table></td></tr></table></body></html>';
+  $userMessage= '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/><title>Yahia Refaiea - Thanks for joining our two weeks web design</title></head><body style="margin: 0;"><table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" style="padding: 84px 63px 28px 63px; background-color: #171a19;"><tr><td align="left" valign="top"><table border="0" cellpadding="0" cellspacing="0" style="min-width: 280px; width: 100%; max-width: 494px; margin: 0 auto;"><tr><td align="left" valign="top"><table border="0" cellpadding="0" cellspacing="0" width="100%" style="margin-bottom: 21px;"><tr><td align="left" valign="top"><a href="https://yahiarefaiea.com" target="_blank" style="display: block; text-decoration: none; width: 100px;"><img src="https://2wwd.nuotron.co/application/includes/images/mails/signature.png" height="28px" style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 400; font-size: 14px; line-height: 28px; color: #fff; white-space: nowrap;" alt="Yahia Refaiea signature" title="Yahia Refaiea signature"/></a></td><td align="right" valign="top"><a href="https://yahiarefaiea.com" target="_blank" style="display: block; text-decoration: none; width: 100px;"><img src="https://2wwd.nuotron.co/application/includes/images/mails/identity.png" height="10px" style="display: block; margin: 9px 0; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 400; font-size: 14px; line-height: 10px; color: #e0a458; white-space: nowrap;" alt="Yahia Refaiea identity" title="Yahia Refaiea identity"/></a></td></tr><tr><td align="left" valign="top" style="padding-top: 42px;"><span style="display: block; font-family: Georgia, sans-serif; font-weight: 700; font-size: 28px; line-height: 42px; color: #fff;">Thanks for joining our two weeks web design</span></td></tr></table><table border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td align="left" valign="top"><span style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 400; font-size: 14px; line-height: 35px; color: #ddd;"><span style="display: block; margin-bottom: 7px;">Hi '. $userName .'!</span><span style="display: block; margin-bottom: 7px;">I'm glad to have the chance to meet such a great person as you in our workshops in September.</span><span style="display: block; margin-bottom: 7px;">I'm so excited. I'll study your application and reach out to you on August 22. We have limited seats in our workshops. So, please stay tuned and confirm your acceptance immediately.</span><span style="display: block;">If you have an emergency or something.. please reach me out at:</span><a href="tel:+963962295406" target="_blank" style="display: block; margin-bottom: 7px; color: #e0a458;">(+963) 96 2295 406</a><span style="display: block;">If it’s not.. go ahead and enjoy these funny cats on YouTube:</span><a href="https://youtu.be/HxM46vRJMZs" target="_blank" style="display: block; color: #e0a458;">https://youtu.be/HxM46vRJMZs</a><span style="display: block; margin-top: 21px; font-size: 14px; line-height: 21px; color: #666;">Best wishes.</span></span></td></tr></table></td></tr></table></td></tr></table></body></html>';
 
 	//	MY MESSAGE
-  $myMessage= '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/><title>Nuotron 2WWD Survey - New Feedback Received</title></head><body style="margin: 0;"><table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" style="padding: 42px 14px 56px 14px; background-color: #eee;"><tr><td align="left" valign="top"><table border="0" cellpadding="0" cellspacing="0" style="min-width: 400px; width: 100%; max-width: 560px; margin: 0 auto;"><tr><td align="left" valign="top"><table border="0" cellpadding="0" cellspacing="0" width="100%" style="margin-bottom: 28px; padding: 0 56px 0 56px;"><tr><td align="left" valign="top"><a href="https://2wwd.nuotron.co/survey/" target="_blank" style="display: block; text-decoration: none; width: 70px;"><img src="https://2wwd.nuotron.co/survey/includes/images/mails/nuotron.png" height="70px" style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 700; font-size: 14px; line-height: 70px; color: #727272; white-space: nowrap;" alt="Nuotron Logo" title="Nuotron Logo"/></a></td></tr><tr><td align="left" valign="top" style="padding-top: 21px;"><span style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 400; font-size: 16px; line-height: 28px; color: #464646;"><span>Hello <span style="font-weight: 700;">Yahia Refaiea</span>,<br/>You have a new feedback to look at</span></span></td></tr></table><table border="0" cellpadding="0" cellspacing="0" width="100%" style="background-color: #f35635;"><tr><td align="center" valign="top"><img src="https://2wwd.nuotron.co/survey/includes/images/mails/nuotron-cover.png" width="100%" style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 700; font-size: 28px; line-height: 210px; color: rgba(0, 0, 0, .21);" alt="Nuotron Cover" title="Nuotron Cover"/></td></tr></table><table border="0" cellpadding="0" cellspacing="0" width="100%" style="margin-bottom: 21px; padding: 28px 56px 28px 56px; background-color: #fff;"><tr><td align="left" valign="top"><span style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 400; font-size: 14px; line-height: 28px; color: #727272;">';
+  $myMessage= '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/><title>Nuotron 2WWD - New Application Received</title></head><body style="margin: 0;"><table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" style="padding: 42px 14px 56px 14px; background-color: #eee;"><tr><td align="left" valign="top"><table border="0" cellpadding="0" cellspacing="0" style="min-width: 400px; width: 100%; max-width: 560px; margin: 0 auto;"><tr><td align="left" valign="top"><table border="0" cellpadding="0" cellspacing="0" width="100%" style="margin-bottom: 28px; padding: 0 56px 0 56px;"><tr><td align="left" valign="top"><a href="https://2wwd.nuotron.co/application/" target="_blank" style="display: block; text-decoration: none; width: 70px;"><img src="https://2wwd.nuotron.co/application/includes/images/mails/nuotron.png" height="70px" style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 700; font-size: 14px; line-height: 70px; color: #727272; white-space: nowrap;" alt="Nuotron Logo" title="Nuotron Logo"/></a></td></tr><tr><td align="left" valign="top" style="padding-top: 21px;"><span style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 400; font-size: 16px; line-height: 28px; color: #464646;"><span>Hello <span style="font-weight: 700;">Yahia Refaiea</span>,<br/>You have a new application to look at</span></span></td></tr></table><table border="0" cellpadding="0" cellspacing="0" width="100%" style="background-color: #f35635;"><tr><td align="center" valign="top"><img src="https://2wwd.nuotron.co/application/includes/images/mails/nuotron-cover.png" width="100%" style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 700; font-size: 28px; line-height: 210px; color: rgba(0, 0, 0, .21);" alt="Nuotron Cover" title="Nuotron Cover"/></td></tr></table><table border="0" cellpadding="0" cellspacing="0" width="100%" style="margin-bottom: 21px; padding: 28px 56px 28px 56px; background-color: #fff;"><tr><td align="left" valign="top"><span style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 400; font-size: 14px; line-height: 28px; color: #727272;">';
 
 	if ($userName === '') {
 		$userName= "Someone";
 	}
-  $mySubject= "Nuotron Survey - New Feedback from ". $userName;
-  $myMessage.= '<span style="display: block; margin-bottom: 21px;">'. $userName .' has just sent a feedback to the Nuotron 2WWD survey.</span>';
+  $mySubject= "Nuotron 2WWD - New Application Received";
+  $myMessage.= '<span style="display: block; margin-bottom: 21px;">'. $userName .' has just applied to the Nuotron 2WWD application.</span>';
 
-  $myMessage.= '<span style="display: block; margin-bottom: 7px; font-size: 12px; line-height: 21px; color: #929292;">01. '. $data[0]->question .'</span>';
-  $myMessage.=                                        '<span style="display: block; margin-bottom: 21px; color: #545454;">'. $data[0]->answer .'</span>';
+	if ($data[0]->answer !== 'Passed empty..') {
+	  $myMessage.= '<span style="display: block; margin-bottom: 7px; font-size: 12px; line-height: 21px; color: #929292;">01. '. $data[0]->question .'</span>';
+	  $myMessage.=                                        '<span style="display: block; margin-bottom: 21px; color: #545454;">'. $data[0]->answer .'</span>';
+	}
 
 	if ($data[1]->answer !== 'Passed empty..') {
 	  $myMessage.= '<span style="display: block; margin-bottom: 7px; font-size: 12px; line-height: 21px; color: #929292;">02. '. $data[1]->question .'</span>';
@@ -78,12 +79,47 @@ if (isset($_POST["dd"])) {
 	  $myMessage.=                                        '<span style="display: block; margin-bottom: 21px; color: #545454;">'. $data[9]->answer .'</span>';
 	}
 
-  $myMessage.= '<span style="display: block;">Keep up the good work,<br/>Team Nuotron</span></span></td></tr></table><table border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td align="center" valign="top"><a href="https://2wwd.nuotron.co/survey/" target="_blank" style="display: block; text-decoration: none; width: 141px;"><img src="https://2wwd.nuotron.co/survey/includes/images/mails/nuotron-flat.png" height="35px" style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 700; font-size: 14px; line-height: 35px; color: #727272;" alt="Nuotron Logo" title="Nuotron Logo"/></a></td></tr><tr><td align="center" valign="top" style="padding-top: 3px;"><span style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 400; font-size: 12px; line-height: 21px; color: #929292;">reach me out through: <br/><a href="https://yahiarefaiea.com" target="_blank" style="color: #929292;">yahiarefaiea.com</a></span></td></tr></table></td></tr></table></td></tr></table></body></html>';
+	if ($data[10]->answer !== 'Passed empty..') {
+	  $myMessage.= '<span style="display: block; margin-bottom: 7px; font-size: 12px; line-height: 21px; color: #929292;">11. '. $data[10]->question .'</span>';
+	  $myMessage.=                                        '<span style="display: block; margin-bottom: 21px; color: #545454;">'. $data[10]->answer .'</span>';
+	}
+
+	if ($data[11]->answer !== 'Passed empty..') {
+	  $myMessage.= '<span style="display: block; margin-bottom: 7px; font-size: 12px; line-height: 21px; color: #929292;">12. '. $data[11]->question .'</span>';
+	  $myMessage.=                                        '<span style="display: block; margin-bottom: 21px; color: #545454;">'. $data[11]->answer .'</span>';
+	}
+
+	if ($data[12]->answer !== 'Passed empty..') {
+	  $myMessage.= '<span style="display: block; margin-bottom: 7px; font-size: 12px; line-height: 21px; color: #929292;">13. '. $data[12]->question .'</span>';
+	  $myMessage.=                                        '<span style="display: block; margin-bottom: 21px; color: #545454;">'. $data[12]->answer .'</span>';
+	}
+
+	if ($data[13]->answer !== 'Passed empty..') {
+	  $myMessage.= '<span style="display: block; margin-bottom: 7px; font-size: 12px; line-height: 21px; color: #929292;">14. '. $data[13]->question .'</span>';
+	  $myMessage.=                                        '<span style="display: block; margin-bottom: 21px; color: #545454;">'. $data[13]->answer .'</span>';
+	}
+
+	if ($data[14]->answer !== 'Passed empty..') {
+	  $myMessage.= '<span style="display: block; margin-bottom: 7px; font-size: 12px; line-height: 21px; color: #929292;">15. '. $data[14]->question .'</span>';
+	  $myMessage.=                                        '<span style="display: block; margin-bottom: 21px; color: #545454;">'. $data[14]->answer .'</span>';
+	}
+
+	if ($data[15]->answer !== 'Passed empty..') {
+	  $myMessage.= '<span style="display: block; margin-bottom: 7px; font-size: 12px; line-height: 21px; color: #929292;">16. '. $data[15]->question .'</span>';
+	  $myMessage.=                                        '<span style="display: block; margin-bottom: 21px; color: #545454;">'. $data[15]->answer .'</span>';
+	}
+
+	if ($data[16]->answer !== 'Passed empty..') {
+	  $myMessage.= '<span style="display: block; margin-bottom: 7px; font-size: 12px; line-height: 21px; color: #929292;">17. '. $data[16]->question .'</span>';
+	  $myMessage.=                                        '<span style="display: block; margin-bottom: 21px; color: #545454;">'. $data[16]->answer .'</span>';
+	}
+
+  $myMessage.= '<span style="display: block;">Keep up the good work,<br/>Team Nuotron</span></span></td></tr></table><table border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td align="center" valign="top"><a href="https://2wwd.nuotron.co/application/" target="_blank" style="display: block; text-decoration: none; width: 141px;"><img src="https://2wwd.nuotron.co/application/includes/images/mails/nuotron-flat.png" height="35px" style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 700; font-size: 14px; line-height: 35px; color: #727272;" alt="Nuotron Logo" title="Nuotron Logo"/></a></td></tr><tr><td align="center" valign="top" style="padding-top: 3px;"><span style="display: block; font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; font-weight: 400; font-size: 12px; line-height: 21px; color: #929292;">reach me out through: <br/><a href="https://yahiarefaiea.com" target="_blank" style="color: #929292;">yahiarefaiea.com</a></span></td></tr></table></td></tr></table></td></tr></table></body></html>';
 
 	//	SEND MAILS
-	// if ($userEmail !== 'Passed empty..') {
-	//   mail($userEmail,$userSubject,$userMessage,$headers);
-	// }
+	if ($userEmail !== 'Passed empty..') {
+	  mail($userEmail,$userSubject,$userMessage,$headers);
+	}
   mail($myEmail,$mySubject,$myMessage,$headers);
 
 	//  RESPONSE
